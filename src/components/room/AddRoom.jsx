8f60551ca86d7e05c90ad3728a7436d1d567fd12
sandesh <bhujbalsandesh52@gmail.com>
2024-01-19 23:17:33 +0530

    added API for select room

--- src/components/room/AddRoom.jsx
+++ src/components/room/AddRoom.jsx
@@ -1,7 +1,8 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react"
 import { addRoom } from "../utils/ApiFunctions"
 import RoomTypeSelector from "../common/RoomTypeSelector"
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
 
 const AddRoom = () => {
 	const [newRoom, setNewRoom] = useState({
@@ -54,26 +55,79 @@ const AddRoom = () => {
 		}, 3000)
 	}
 
-  return (
-    <>
-        <section >
-          <div>
-            <div>
-              <h2>Add New Room</h2>
+	return (
+		<>
+			<section className="container mt-5 mb-5">
+				<div className="row justify-content-center">
+					<div className="col-md-8 col-lg-6">
+						<h2 className="mt-5 mb-2">Add a New Room</h2>
+						{successMessage && (
+							<div className="alert alert-success fade show"> {successMessage}</div>
+						)}
 
+						{errorMessage && <div className="alert alert-danger fade show"> {errorMessage}</div>}
 
+						<form onSubmit={handleSubmit}>
+							<div className="mb-3">
+								<label htmlFor="roomType" className="form-label">
+									Room Type
+								</label>
+								<div>
+									<RoomTypeSelector
+										handleRoomInputChange={handleRoomInputChange}
+										newRoom={newRoom}
+									/>
+								</div>
+							</div>
+							<div className="mb-3">
+								<label htmlFor="roomPrice" className="form-label">
+									Room Price
+								</label>
+								<input
+									required
+									type="number"
+									className="form-control"
+									id="roomPrice"
+									name="roomPrice"
+									value={newRoom.roomPrice}
+									onChange={handleRoomInputChange}
+								/>
+							</div>
 
-            </div>
-
-
-            
-          </div>
-
-
-        </section>
-    </>
-  )
+							<div className="mb-3">
+								<label htmlFor="photo" className="form-label">
+									Room Photo
+								</label>
+								<input
+									required
+									name="photo"
+									id="photo"
+									type="file"
+									className="form-control"
+									onChange={handleImageChange}
+								/>
+								{imagePreview && (
+									<img
+										src={imagePreview}
+										alt="Preview  room photo"
+										style={{ maxWidth: "400px", maxHeight: "400px" }}
+										className="mb-3"></img>
+								)}
+							</div>
+							<div className="d-grid gap-2 d-md-flex mt-2">
+								{/* <Link to={"/existing-rooms"} className="btn btn-outline-info">
+									Existing rooms
+								</Link> */}
+								<button type="submit" className="btn btn-outline-primary ml-5">
+									Save Room
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</section>
+		</>
+	)
 }
-
 
 export default AddRoom
--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -1,30 +1,33 @@
-import axios from "axios";
+import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080",
-});
+	baseURL: "http://localhost:8080"
+})
 
-// This function add a new room to the database
+/* This function adds a new room room to the database */
 export async function addRoom(photo, roomType, roomPrice) {
-  const formData = new FormData()
-  formData.append("photo", photo)
-  formData.append("roomType", roomType)
-  formData.append("roomPrice", roomPrice)
+	const formData = new FormData()
+	formData.append("photo", photo)
+	formData.append("roomType", roomType)
+	formData.append("roomPrice", roomPrice)
 
-  const response = await api.post("/rooms/add/new-room", formData)
-  if(response.status === 201){
-    return true
-  }else{
-    return false
-  }
+	const response = await api.post("/rooms/add/new-room", formData,{
+		// eslint-disable-next-line no-undef
+		headers: getHeader()
+	})
+	if (response.status === 201) {
+		return true
+	} else {
+		return false
+	}
 }
 
-//  This functions get all room types from the database
-export async function getRoomTypes(){
-    try {
-        const response = await api.get("/rooms/room-types")
-        return response.data
-    } catch (error) {
-        throw new Error("Error fetching room types")
-    }
+/* This function gets all room types from thee database */
+export async function getRoomTypes() {
+	try {
+		const response = await api.get("/rooms/room/types")
+		return response.data
+	} catch (error) {
+		throw new Error("Error fetching room types")
+	}
 }
