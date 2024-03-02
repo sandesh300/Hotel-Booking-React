c6698fbe3130a30a92da541c36a18b4642b434ce
sandesh <bhujbalsandesh52@gmail.com>
2024-03-02 15:30:26 +0530

    update Room API's

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react"
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
-import './App.css'
-import AddRoom from "./components/room/AddRoom"
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddRoom from "./components/room/AddRoom";
+import ExistingRooms from "./components/room/ExistingRooms";
+import Home from "./components/home/Home";
+import EditRoom from "./components/room/EditRoom";
 
 function App() {
-
   return (
     <>
-  <AddRoom />
-   </>
-  )
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/existing-rooms" element={<ExistingRooms />} />
+          </Routes>
+        </Router>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
