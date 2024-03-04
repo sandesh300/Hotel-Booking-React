c99473134e7f42ae02d7df0f92af33760f737075
sandesh <bhujbalsandesh52@gmail.com>
2024-03-04 21:06:05 +0530

    create Navbar file

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,23 @@ import AddRoom from "./components/room/AddRoom";
 import ExistingRooms from "./components/room/ExistingRooms";
 import Home from "./components/home/Home";
 import EditRoom from "./components/room/EditRoom";
+import Footer from "./components/layout/Footer";
+import NavBar from "./components/layout/Navbar";
 
 function App() {
   return (
     <>
       <main>
-      <Router>
+        <Router>
+          <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/edit-room/:roomId" element={<EditRoom />} />
             <Route path="/existing-rooms" element={<ExistingRooms />} />
             <Route path="/add-room" element={<AddRoom />} />
-
           </Routes>
         </Router>
+        <Footer />
       </main>
     </>
   );
