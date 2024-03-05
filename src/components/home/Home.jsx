beab9351707559731ac4dcde1cbfe2b9e150bfed
sandesh <bhujbalsandesh52@gmail.com>
2024-03-05 23:16:32 +0530

    craeat Room Carousel component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import MainHeader from '../layout/MainHeader'
+import Parallax from '../common/Parallax'
+import HotelService from '../common/HotelService'
 
 const Home = () => {
   return (
-    <div>
-        <h2>Welcome To the Home page</h2>
-    </div>
+    <section>
+
+    <MainHeader />
+    <section className='container'>
+<Parallax />
+<HotelService />
+<Parallax />
+
+    </section>
+
+    </section>
   )
 }
 
