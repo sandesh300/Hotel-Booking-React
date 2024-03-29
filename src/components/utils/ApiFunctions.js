e97e03dbbffd6b31a885db253f36e7cec17c2b8d
sandesh <bhujbalsandesh52@gmail.com>
2024-03-29 18:50:14 +0530

    add Registration and Login API in utils

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -140,3 +140,32 @@ export async function getAvailableRooms(checkInDate, checkOutDate, roomType) {
 	)
 	return result
 }
+
+/* This function register a new user */
+export async function registerUser(registration) {
+	try {
+		const response = await api.post("/auth/register-user", registration)
+		return response.data
+	} catch (error) {
+		if (error.reeponse && error.response.data) {
+			throw new Error(error.response.data)
+		} else {
+			throw new Error(`User registration error : ${error.message}`)
+		}
+	}
+}
+
+/* This function login a registered user */
+export async function loginUser(login) {
+	try {
+		const response = await api.post("/auth/login", login)
+		if (response.status >= 200 && response.status < 300) {
+			return response.data
+		} else {
+			return null
+		}
+	} catch (error) {
+		console.error(error)
+		return null
+	}
+}
